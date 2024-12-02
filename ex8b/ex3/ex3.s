c7f7da94e43602b4333cbe6339a5dddcd2e74d80
Farkhodov Abdulaziz <116057829+Farkhodov721@users.noreply.github.com>
2024-12-02 16:40:40 +0000

    ex8b -> ex3 code updated

--- ex8b/ex3/ex3.s
+++ ex8b/ex3/ex3.s
@@ -1,32 +1,31 @@
 .global find_min_max
 
 find_min_max:
-    PUSH {R4, R5, LR}
-    MOV  R4, R0
-    LDR  R0, [R4]
-    MOV  R1, R0
-    MOV  R5, R1
+    PUSH {R4, R5, R6, LR}           
+    MOV  R4, R0                     
+    LDR  R0, [R4]                  
+    MOV  R1, R0                     
+    MOV  R5, #4                     
+loop:
+    LDR  R6, [R4], #4               
+    CMP  R6, R0                    
+    MOVLT R0, R6                    
+    CMP  R6, R1                     
+    SUBS R5, R5, #1                
+    BNE  loop                       
 
-1:  LDR  R6, [R4], #4
-    CMP  R6, R0
-    MOVLT R0, R6
-    CMP  R6, R1
-    MOVGT R1, R6
-    SUBS R5, R5, #1
-    BNE  1b
-
-    POP {R4, R5, LR}
-    BX  LR
+    POP {R4, R5, R6, LR}           
+    BX   LR                        
 
 .global _start
 
 _start:
-    LDR  R0, =array
-    MOV  R1, #5
-    BL   find_min_max
+    LDR  R0, =array                 
+    MOV  R1, #5                    
+    BL   find_min_max              
 
-    MOV  R2, R0
-    MOV  R3, R1
+    MOV  R2, R0                     
+    MOV  R3, R1                     
 
 .data
-array: .word 3, 1, 4, 1, 5
+array: .word 3, 1, 4, 1, 5        
