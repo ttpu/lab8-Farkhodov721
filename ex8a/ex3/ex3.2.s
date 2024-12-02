0d0ec888844ad442de71f946cc724e561841a9fc
Farkhodov Abdulaziz <116057829+Farkhodov721@users.noreply.github.com>
2024-12-02 11:43:56 +0000

    ex 3.2 finished

--- ex8a/ex3/ex3.2.s
+++ ex8a/ex3/ex3.2.s
@@ -1,11 +1,10 @@
 .global _start
 _start:
     MOV     R0, #0
-    LDR     R1, =0x40000000
+    MOV     R2, #0
 
 loop:
-    STR     R0, [R1]
+    ADD     R2, R2, R0
     ADD     R0, R0, #1
-    ADD     R1, R1, #4
-    CMP     R0, #10
+    CMP     R0, R1
     BLT     loop
